9c5469aaba547e85fd935729d6067b6e31bf11c5
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-03 16:58:21 +0800

    Add fluidSize to height

--- src/components/bg-image/bg-image.styles.tsx
+++ src/components/bg-image/bg-image.styles.tsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
 import bgImageJPG from '../../assets/bg-desktop-light.jpg';
+import { BREAKPOINTS } from '../../styles/breakpoints';
+import fluidSize from '../../styles/fluidSize';
 
 export const StyledBGImage = styled.div`
   width: 100%;
-  height: 300px;
+  height: ${fluidSize(100, 300, BREAKPOINTS.UP.MOBILE, BREAKPOINTS.UP.DESKTOP)};
   background: url(${bgImageJPG}) no-repeat center;
   background-size: auto;
 `;

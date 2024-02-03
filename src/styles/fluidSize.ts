8d87900e10f11c3d9200aeaa7fff8d189dc7d31d
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-03 16:45:10 +0800

    Implement fluidSize function for smooth view width transition

---
+++ src/styles/fluidSize.ts
@@ -0,0 +1,20 @@
+import { BreakpointUp } from './breakpoints';
+import pxToRem from './pxToRem';
+
+const fluidSize = (
+  minSize: number,
+  maxSize: number,
+  minVW: BreakpointUp,
+  maxVW: BreakpointUp,
+): string => {
+  const minSizeRem = pxToRem(minSize);
+  const maxSizeRem = pxToRem(maxSize);
+
+  const slope = (maxSize - minSize) / (maxVW - minVW);
+
+  const fluidSize = `calc(${slope} * (100vw + ${minSizeRem}))`;
+
+  return `clamp(${minSizeRem}, ${fluidSize}, ${maxSizeRem})`;
+};
+
+export default fluidSize;

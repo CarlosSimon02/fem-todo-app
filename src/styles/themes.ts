7ff624891b0eba30676b5c525ce78c893a2632ae
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-04 21:20:57 +0800

    Add theme constants

---
+++ src/styles/themes.ts
@@ -0,0 +1,14 @@
+export const DARKTHEME = {
+  name: 'dark',
+  colors: {
+    body: {
+      background: 'white',
+      foreground: 'black',
+    },
+  },
+};
+
+export const LIGHTTHEME = {
+  name: 'light',
+  colors: {},
+};

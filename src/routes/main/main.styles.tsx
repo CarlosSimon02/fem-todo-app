0283ff2354e396b434ac6d40f9603df90ab21cb2
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-05 21:44:02 +0800

    Add fluid styling

--- src/routes/main/main.styles.tsx
+++ src/routes/main/main.styles.tsx
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 
-import { fluidSizeMobileToDesktop } from '../../styles/fluidSize';
-import pxToRem from '../../styles/pxToRem';
+import { fluidSizeMobileToDesktop } from '../../styles/functions/fluidSize';
+import pxToRem from '../../styles/functions/pxToRem';
 
 export const StyledMainWrapper = styled.div`
   width: calc(100vw - ${pxToRem(48)});
   max-width: ${pxToRem(584)};
   margin-inline: auto;
   margin-block-start: ${fluidSizeMobileToDesktop(48, 70)};
-  padding-inline: ${pxToRem(24)};
 `;

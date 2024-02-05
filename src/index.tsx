8ac99111d129cd2fc293ed01e39946f49dfe1648
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-05 21:41:27 +0800

    Add theme switching

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import { CurrentThemeProvider } from './contexts/current-theme.context';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <CurrentThemeProvider>
+      <App />
+    </CurrentThemeProvider>
   </React.StrictMode>,
 );
 

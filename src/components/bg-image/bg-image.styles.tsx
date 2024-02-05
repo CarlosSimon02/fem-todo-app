63aaac02ab7b160cc3a6661bf6cfc051f93e97d4
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-05 21:41:09 +0800

    Add theme switching

--- src/components/bg-image/bg-image.styles.tsx
+++ src/components/bg-image/bg-image.styles.tsx
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 
-import bgImageJPG from '../../assets/bg-desktop-light.jpg';
-import { fluidSizeMobileToDesktop } from '../../styles/fluidSize';
+import { fluidSizeMobileToDesktop } from '../../styles/functions/fluidSize';
 
 export const StyledBGImage = styled.div`
   width: 100%;
   height: ${fluidSizeMobileToDesktop(200, 300)};
-  background: url(${bgImageJPG}) no-repeat center;
+  background: url(${({ theme }) => theme.bgImage}) no-repeat center;
   background-size: auto 100%;
   position: absolute;
   top: 0;

16f1be1598a9d31d6b07ac4776d4db772b85f9dc
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-06 21:08:07 +0800

    Update style

--- src/components/create-to-do/create-to-do.styles.tsx
+++ src/components/create-to-do/create-to-do.styles.tsx
@@ -2,13 +2,16 @@ import styled from 'styled-components';
 
 import ItemBox from '../../styles/common/itemBox';
 import MainBox from '../../styles/common/mainBox';
+import { fluidSizeMobileToDesktop } from '../../styles/functions/fluidSize';
 
 export const StyledCreateToDoContainer = styled.div`
   position: relative;
 `;
 
 export const StyledCreateToDoInput = styled.input`
-  border: none;
   ${ItemBox}
   ${MainBox}
+  border: none;
+  width: 100%;
+  padding-inline-start: ${fluidSizeMobileToDesktop(52, 72)};
 `;

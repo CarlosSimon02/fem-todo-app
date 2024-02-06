6ed6813ed3d0be3d95051062dce49f01e3bc6797
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-06 21:13:16 +0800

    Update style

--- src/styles/common/itemBox.tsx
+++ src/styles/common/itemBox.tsx
@@ -5,8 +5,9 @@ import { fluidSizeMobileToDesktop } from '../functions/fluidSize';
 const ItemBox = css`
   font-size: ${fluidSizeMobileToDesktop(12, 18)};
   padding-block: ${fluidSizeMobileToDesktop(16, 20)};
-  padding-inline-end: ${fluidSizeMobileToDesktop(20, 24)};
-  padding-inline-start: ${fluidSizeMobileToDesktop(52, 72)};
+  padding-inline: ${fluidSizeMobileToDesktop(20, 24)};
+  text-overflow: ellipsis;
+  line-height: 1.5;
 `;
 
 export default ItemBox;

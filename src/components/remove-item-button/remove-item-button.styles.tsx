a0ebd29a570dddee1550e46d8117dd934ed9d4ee
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-06 21:08:35 +0800

    Create RemoveItemButton component

---
+++ src/components/remove-item-button/remove-item-button.styles.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+import { ReactComponent as CrossSVG } from '../../assets/icon-cross.svg';
+import { fluidSizeMobileToDesktop } from '../../styles/functions/fluidSize';
+import pxToRem from '../../styles/functions/pxToRem';
+
+export const StyledRemoveItemButton = styled.button`
+  cursor: pointer;
+  width: ${fluidSizeMobileToDesktop(12, 18)};
+  height: ${fluidSizeMobileToDesktop(12, 18)};
+  position: absolute;
+  right: ${pxToRem(20)};
+  top: ${fluidSizeMobileToDesktop(20, 23)};
+  background-color: transparent;
+  border: none;
+  padding: 0;
+`;
+
+export const StyledCrossIcon = styled(CrossSVG)`
+  width: 100%;
+  height: 100%;
+  fill: ${({ theme }) => theme.color.crossIcon.fill};
+`;

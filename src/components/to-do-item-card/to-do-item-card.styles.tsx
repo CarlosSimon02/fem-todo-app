30ae231be3931cf9e4c480cf94710f909ffafbd2
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-06 21:09:59 +0800

    Rename ToDoItem component to ToDoItemCard for easy identification

---
+++ src/components/to-do-item-card/to-do-item-card.styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+import ItemBox from '../../styles/common/itemBox';
+import { fluidSizeMobileToDesktop } from '../../styles/functions/fluidSize';
+
+export const StyledToDoItemCardContainer = styled.div`
+  ${ItemBox}
+  position: relative;
+  background-color: ${({ theme }) => theme.color.mainBox.background};
+  padding-inline: ${fluidSizeMobileToDesktop(52, 72)};
+  text-overflow: ellipsis;
+  overflow: hidden;
+`;

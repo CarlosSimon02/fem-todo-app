b12491baa243f88f4348eb0a937f73cc92a55b9e
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-06 21:09:45 +0800

    Rename ToDoItem component to ToDoItemCard for easy identification

--- src/components/to-do-item/to-do-item.styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-import ItemBox from '../../styles/common/itemBox';
-
-export const StyledToDoItemContainer = styled.div`
-  ${ItemBox}
-  position: relative;
-  background-color: ${({ theme }) => theme.color.mainBox.background};
-`;

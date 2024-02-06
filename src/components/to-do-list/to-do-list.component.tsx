4a702227d41cb9ae714a25ff3059a44662a2387d
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-06 21:10:48 +0800

    Map filteredToDoItems to view

--- src/components/to-do-list/to-do-list.component.tsx
+++ src/components/to-do-list/to-do-list.component.tsx
@@ -1,12 +1,23 @@
-import ToDoItem from '../to-do-item/to-do-item.component';
+import { useContext } from 'react';
+
+import { ToDoItemsContext } from '../../contexts/to-do-items.context';
+import ToDoItemCard from '../to-do-item-card/to-do-item-card.component';
 import { StyledControlsContainer, StyledToDoListContainer } from './to-do-list.styles';
 
 const ToDoList = () => {
+  const toDoItemsContext = useContext(ToDoItemsContext);
+
+  if (!toDoItemsContext) {
+    throw new Error('CurrentThemeContext is not provided');
+  }
+
+  const { filteredToDoItems } = toDoItemsContext;
+
   return (
     <StyledToDoListContainer>
-      <ToDoItem />
-      <ToDoItem />
-      <ToDoItem />
+      {filteredToDoItems.map((item) => {
+        return <ToDoItemCard key={item.id} toDoItem={item} />;
+      })}
       <StyledControlsContainer>
         <p>5 items left</p>
         <p>Clear Completed</p>

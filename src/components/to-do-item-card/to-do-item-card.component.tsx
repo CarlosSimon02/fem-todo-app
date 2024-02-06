eca0e196c37a199d0d34e548ddda70d0a4129e74
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-06 21:09:53 +0800

    Rename ToDoItem component to ToDoItemCard for easy identification

---
+++ src/components/to-do-item-card/to-do-item-card.component.tsx
@@ -0,0 +1,39 @@
+import { useContext, useState } from 'react';
+
+import { ToDoItem, ToDoItemsContext } from '../../contexts/to-do-items.context';
+import Checkbox from '../checkbox/checkbox.component';
+import RemoveItemButton from '../remove-item-button/remove-item-button.component';
+import { StyledToDoItemCardContainer } from './to-do-item-card.styles';
+
+type ToDoItemCardProps = {
+  key?: string;
+  toDoItem: ToDoItem;
+};
+
+const ToDoItemCard = ({ toDoItem }: ToDoItemCardProps) => {
+  const { id, isDone, task } = toDoItem;
+  const [isChecked, setIsChecked] = useState(isDone);
+  const toDoItemsContext = useContext(ToDoItemsContext);
+
+  if (!toDoItemsContext) {
+    throw new Error('CurrentThemeContext is not provided');
+  }
+
+  const { updateToDoItem } = toDoItemsContext;
+
+  const toggleCheckHandler = () => {
+    const newIsChecked = !isChecked;
+    setIsChecked(newIsChecked);
+    updateToDoItem({ ...toDoItem, isDone: newIsChecked });
+  };
+
+  return (
+    <StyledToDoItemCardContainer>
+      <Checkbox checked={isChecked} onClick={toggleCheckHandler} />
+      <p>{task}</p>
+      <RemoveItemButton toDoItemID={id} />
+    </StyledToDoItemCardContainer>
+  );
+};
+
+export default ToDoItemCard;

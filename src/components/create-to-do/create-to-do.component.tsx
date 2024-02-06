8ac068285eeb49783c25f1637db2009abb9b49fa
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-06 21:07:54 +0800

    Implement item creation when "Enter" key is press

--- src/components/create-to-do/create-to-do.component.tsx
+++ src/components/create-to-do/create-to-do.component.tsx
@@ -1,11 +1,59 @@
+import { ChangeEvent, KeyboardEvent, useContext, useRef, useState } from 'react';
+
+import { v4 } from 'uuid';
+
+import { ToDoItemsContext } from '../../contexts/to-do-items.context';
 import Checkbox from '../checkbox/checkbox.component';
 import { StyledCreateToDoContainer, StyledCreateToDoInput } from './create-to-do.styles';
 
 const CreateToDo = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const toDoItemsContext = useContext(ToDoItemsContext);
+
+  if (!toDoItemsContext) {
+    throw new Error('ToDoItemsContext is not provided');
+  }
+
+  const { createToDoItem } = toDoItemsContext;
+
+  // handle checkbox click event
+  const toggleCheckHandler = () => setIsChecked((prev) => !prev);
+
+  // Get the value from the input element
+  const inputValueChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  // Add item when "Enter" key is pressed
+  const inputKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (inputValue) {
+      switch (event.key) {
+        case 'Enter':
+          createToDoItem({ id: v4(), isDone: isChecked, task: inputValue });
+
+          // Clear the input value
+          if (inputRef.current) {
+            inputRef.current.value = '';
+            setInputValue('');
+          }
+          break;
+      }
+    }
+  };
+
   return (
     <StyledCreateToDoContainer>
-      <StyledCreateToDoInput type="text" placeholder="Create new to-do..." />
-      <Checkbox />
+      <StyledCreateToDoInput
+        ref={inputRef}
+        type="text"
+        placeholder="Create new to-do..."
+        onChange={inputValueChangeHandler}
+        onKeyDown={inputKeyDownHandler}
+      />
+      <Checkbox checked={isChecked} onClick={toggleCheckHandler} />
     </StyledCreateToDoContainer>
   );
 };

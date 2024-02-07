23c82c354d884bdafa5c580e8fcd1107fbf948f7
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-07 19:49:06 +0800

    Fix bugs: toDoItems not updating properly on updateToDoItem()

--- src/contexts/to-do-items.context.tsx
+++ src/contexts/to-do-items.context.tsx
@@ -2,13 +2,13 @@ import { Dispatch, ReactNode, SetStateAction, createContext, useEffect, useState
 
 export enum FilterType {
   All = 'all',
-  Done = 'done',
+  Completed = 'completed',
   Active = 'active',
 }
 
 export type ToDoItem = {
   id: string;
-  isDone: boolean;
+  isCompleted: boolean;
   task: string;
 };
 
@@ -16,10 +16,13 @@ type ToDoItemsContextProps = {
   toDoItems: ToDoItem[];
   filteredToDoItems: ToDoItem[];
   currentFilter: FilterType;
+  activeItemsCount: number;
+  doneItemsCount: number;
   setCurrentFilter: Dispatch<SetStateAction<FilterType>>;
   createToDoItem: (toAddItem: ToDoItem) => void;
   updateToDoItem: (toUpdateItem: ToDoItem) => void;
   deleteToDoItem: (toDeleteIDItem: string) => void;
+  clearCompleted: () => void;
 };
 
 type ToDoItemsProviderProps = {
@@ -31,18 +34,24 @@ export const ToDoItemsContext = createContext<ToDoItemsContextProps | undefined>
 export const ToDoItemsProvider = ({ children }: ToDoItemsProviderProps) => {
   const [toDoItems, setToDoItems] = useState<ToDoItem[]>([]);
   const [filteredToDoItems, setFilteredToDoItems] = useState<ToDoItem[]>([]);
+  const [activeItemsCount, setActiveItemsCount] = useState(0);
+  const [doneItemsCount, setCompletedItemsCount] = useState(0);
   const [currentFilter, setCurrentFilter] = useState<FilterType>(FilterType.All);
 
   useEffect(() => {
+    console.log('run');
+    setActiveItemsCount(toDoItems.reduce((sum, item) => sum + (!item.isCompleted ? 1 : 0), 0));
+    setCompletedItemsCount(toDoItems.reduce((sum, item) => sum + (item.isCompleted ? 1 : 0), 0));
+
     switch (currentFilter) {
       case FilterType.All:
         setFilteredToDoItems(toDoItems);
         return;
       case FilterType.Active:
-        setFilteredToDoItems(toDoItems.filter((item) => !item.isDone));
+        setFilteredToDoItems(toDoItems.filter((item) => !item.isCompleted));
         return;
-      case FilterType.Done:
-        setFilteredToDoItems(toDoItems.filter((item) => item.isDone));
+      case FilterType.Completed:
+        setFilteredToDoItems(toDoItems.filter((item) => item.isCompleted));
         return;
     }
   }, [toDoItems, currentFilter]);
@@ -56,8 +65,9 @@ export const ToDoItemsProvider = ({ children }: ToDoItemsProviderProps) => {
 
     if (indexToUpdate !== -1) {
       setToDoItems((prevToDoItems) => {
-        prevToDoItems[indexToUpdate] = toUpdateItem;
-        return prevToDoItems;
+        const updatedItems = [...prevToDoItems];
+        updatedItems[indexToUpdate] = toUpdateItem;
+        return updatedItems;
       });
     } else {
       throw new Error(`Object with ID ${toUpdateItem.id} not found.`);
@@ -68,20 +78,27 @@ export const ToDoItemsProvider = ({ children }: ToDoItemsProviderProps) => {
     const indexToDelete = toDoItems.findIndex((item) => item.id === toDeleteIDItem);
 
     if (indexToDelete !== -1) {
-      setToDoItems(toDoItems.filter((item) => item.id !== toDeleteIDItem));
+      setToDoItems((prevToDoItems) => prevToDoItems.filter((item) => item.id !== toDeleteIDItem));
     } else {
       throw new Error(`Object with ID ${toDeleteIDItem} not found.`);
     }
+  };
+
+  const clearCompleted = () => {
+    setToDoItems(toDoItems.filter((item) => !item.isCompleted));
   };
 
   const contextValue = {
     toDoItems,
     filteredToDoItems,
     currentFilter,
+    activeItemsCount,
+    doneItemsCount,
     setCurrentFilter,
     createToDoItem,
     updateToDoItem,
     deleteToDoItem,
+    clearCompleted,
   };
 
   return <ToDoItemsContext.Provider value={contextValue}>{children}</ToDoItemsContext.Provider>;

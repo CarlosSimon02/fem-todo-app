cc1ba996b8b98c7281cd81b1c65b43a95d720504
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-07 08:58:15 +0800

    Add placeholder style

--- src/components/create-to-do/create-to-do.styles.tsx
+++ src/components/create-to-do/create-to-do.styles.tsx
@@ -14,4 +14,10 @@ export const StyledCreateToDoInput = styled.input`
   border: none;
   width: 100%;
   padding-inline-start: ${fluidSizeMobileToDesktop(52, 72)};
+  caret-color: ${({ theme }) => theme.color.accent.main};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.color.inputText.placeholder};
+    opacity: 1;
+  }
 `;

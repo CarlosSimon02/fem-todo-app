2f7eef7d84294a67b34dfc18370060b61c4f239d
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-08 22:12:42 +0800

    Implement responsive rendering to where component will be place

--- src/routes/home/home.route.tsx
+++ src/routes/home/home.route.tsx
@@ -1,12 +1,18 @@
 import CreateToDo from '../../components/create-to-do/create-to-do.component';
+import FilterBar from '../../components/filter-bar/filter-bar.component';
 import ToDoList from '../../components/to-do-list/to-do-list.component';
+import useWindow from '../../hooks/useWindow.hook';
+import { BREAKPOINTS } from '../../styles/constants/breakpoints';
 import { StyledHomeContainer } from './home.styles';
 
 const Home = () => {
+  const { innerWidth } = useWindow();
+
   return (
     <StyledHomeContainer>
       <CreateToDo />
       <ToDoList />
+      {innerWidth < BREAKPOINTS.UP.TABLET && <FilterBar />}
     </StyledHomeContainer>
   );
 };

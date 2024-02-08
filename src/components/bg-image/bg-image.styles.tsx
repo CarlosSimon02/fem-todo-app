0c1d19af4ab4b199c4a7792415cd2d9e43514405
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-08 22:08:54 +0800

    Seperate light and dark mode image to div

--- src/components/bg-image/bg-image.styles.tsx
+++ src/components/bg-image/bg-image.styles.tsx
@@ -1,14 +1,36 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
+import bgImageDarkJPG from '../../assets/bg-desktop-dark.jpg';
+import bgImageLightJPG from '../../assets/bg-desktop-light.jpg';
 import { fluidSizeMobileToDesktop } from '../../styles/functions/fluidSize';
 
-export const StyledBGImage = styled.div`
+export const StyledBGImageContainer = styled.div`
   width: 100%;
   height: ${fluidSizeMobileToDesktop(200, 300)};
-  background: url(${({ theme }) => theme.bgImage}) no-repeat center;
-  background-size: auto 100%;
+
   position: absolute;
   top: 0;
   left: 0;
   z-index: -99;
+`;
+
+const StyledImage = css`
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: auto 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+export const LightModeImage = styled.div`
+  ${StyledImage}
+  background-image: url(${bgImageLightJPG});
+`;
+
+export const DarkModeImage = styled.div`
+  ${StyledImage}
+  background-image: url(${bgImageDarkJPG});
 `;

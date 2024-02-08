b720ea618338335ab2dd427ff323a470d991bc6c
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-08 22:08:26 +0800

    Add fade-in fade-out effect using framer-motion

--- src/components/bg-image/bg-image.component.tsx
+++ src/components/bg-image/bg-image.component.tsx
@@ -1,7 +1,32 @@
-import { StyledBGImage } from './bg-image.styles';
+import { AnimatePresence, motion } from 'framer-motion';
+
+import { ThemeType } from '../../contexts/current-theme.context';
+import useCurrentTheme from '../../hooks/useCurrentTheme.hook';
+import { DarkModeImage, LightModeImage, StyledBGImageContainer } from './bg-image.styles';
+
+const AnimateDarkModeImage = motion(DarkModeImage);
+const AnimateLightModeImage = motion(LightModeImage);
+
+const imageAnimateProps = {
+  transition: { duration: 0.7 },
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+};
 
 const BGImage = () => {
-  return <StyledBGImage />;
+  const { currentTheme } = useCurrentTheme();
+  return (
+    <StyledBGImageContainer>
+      <AnimatePresence initial={false}>
+        {currentTheme === ThemeType.Light ? (
+          <AnimateLightModeImage key="dark-image" {...imageAnimateProps} />
+        ) : (
+          <AnimateDarkModeImage key="light-image" {...imageAnimateProps} />
+        )}
+      </AnimatePresence>
+    </StyledBGImageContainer>
+  );
 };
 
 export default BGImage;

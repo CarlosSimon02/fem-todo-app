d063484ee2e7dd7a2cd55c753ee1e7d8553a9156
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-08 22:11:13 +0800

    Add effects and transitions using framer-motion

--- src/components/theme-switcher/theme-switcher.component.tsx
+++ src/components/theme-switcher/theme-switcher.component.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import { ThemeType } from '../../contexts/current-theme.context';
 import useCurrentTheme from '../../hooks/useCurrentTheme.hook';
 import { StyledMoonIcon, StyledSunIcon, StyledThemeSwitcherButton } from './theme-switcher.styles';
 
-const ThemeSwitcher: React.FC = () => {
+const AnimatedThemeSwitcherButton = motion(StyledThemeSwitcherButton);
+const AnimatedMoonIcon = motion(StyledMoonIcon);
+const AnimatedSunIcon = motion(StyledSunIcon);
+
+const iconAnimateProps = {
+  transition: { duration: 0.7 },
+  initial: { opacity: 0, rotate: 120 },
+  animate: { opacity: 1, rotate: 0 },
+  exit: { opacity: 0, rotate: -120 },
+};
+
+const ThemeSwitcher = () => {
   const { currentTheme, setCurrentTheme } = useCurrentTheme();
 
   const toggleThemeHandler = () => {
@@ -12,19 +23,21 @@ const ThemeSwitcher: React.FC = () => {
   };
 
   return (
-    <StyledThemeSwitcherButton onClick={toggleThemeHandler}>
-      {currentTheme === ThemeType.Light ? (
-        <>
-          <StyledMoonIcon />
-          <p className="sr-only">Toggle to Dark Mode</p>
-        </>
-      ) : (
-        <>
-          <StyledSunIcon />
-          <p className="sr-only">Toggle to Light Mode</p>
-        </>
-      )}
-    </StyledThemeSwitcherButton>
+    <AnimatedThemeSwitcherButton
+      onTap={toggleThemeHandler}
+      whileHover={{ scale: 1.4, transition: { duration: 0.4 } }}
+      whileTap={{ scale: 0.8, transition: { duration: 0.7 } }}
+      transition={{ ease: 'easeOut', duration: 1 }}
+    >
+      <AnimatePresence initial={false}>
+        {currentTheme === ThemeType.Light ? (
+          <AnimatedMoonIcon key="moon" {...iconAnimateProps} />
+        ) : (
+          <AnimatedSunIcon key="sun" {...iconAnimateProps} />
+        )}
+      </AnimatePresence>
+      <p className="sr-only">Toggle to {currentTheme as string} mode</p>
+    </AnimatedThemeSwitcherButton>
   );
 };
 

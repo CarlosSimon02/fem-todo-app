4e4bc9d34954839ce5dbfd27e0b1402b9228ca8f
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-12 11:59:33 +0800

    Change input radio to button

--- src/components/filter-bar/filter-bar.component.tsx
+++ src/components/filter-bar/filter-bar.component.tsx
@@ -1,57 +1,43 @@
-import { ChangeEvent } from 'react';
+import { MouseEvent } from 'react';
 
 import { FilterType } from '../../contexts/to-do-items.context';
 import useToDoItems from '../../hooks/useToDoItems.hook';
 import useWindow from '../../hooks/useWindow.hook';
 import { BREAKPOINTS } from '../../styles/constants/breakpoints';
-import { StyledFieldSet, StyledLabel } from './filter-bar.styles';
+import { StyledButton, StyledFieldSet } from './filter-bar.styles';
 
 const FilterBar = () => {
   const { innerWidth } = useWindow();
   const { currentFilter, setCurrentFilter } = useToDoItems();
 
-  const inputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      setCurrentFilter(event.target.value as FilterType);
-    }
+  const clickHandler = (event: MouseEvent<HTMLButtonElement>): void => {
+    setCurrentFilter(event.currentTarget.dataset.filter as FilterType);
   };
 
   return (
     <StyledFieldSet className={innerWidth < BREAKPOINTS.UP.TABLET ? 'isolated' : 'integrated'}>
       <legend className="sr-only">Choose a filter</legend>
-      <StyledLabel>
-        <input
-          className="sr-only"
-          type="radio"
-          name="filter"
-          value={FilterType.All}
-          checked={FilterType.All === currentFilter}
-          onChange={inputChangeHandler}
-        />
-        <p>All</p>
-      </StyledLabel>
-      <StyledLabel>
-        <input
-          className="sr-only"
-          type="radio"
-          name="filter"
-          value={FilterType.Active}
-          checked={FilterType.Active === currentFilter}
-          onChange={inputChangeHandler}
-        />
-        <p>Active</p>
-      </StyledLabel>
-      <StyledLabel>
-        <input
-          className="sr-only"
-          type="radio"
-          name="filter"
-          value={FilterType.Completed}
-          checked={FilterType.Completed === currentFilter}
-          onChange={inputChangeHandler}
-        />
-        <p>Completed</p>
-      </StyledLabel>
+      <StyledButton
+        className={currentFilter === FilterType.All ? 'current-filter' : ''}
+        data-filter="all"
+        onClick={clickHandler}
+      >
+        All
+      </StyledButton>
+      <StyledButton
+        className={currentFilter === FilterType.Active ? 'current-filter' : ''}
+        data-filter="active"
+        onClick={clickHandler}
+      >
+        Active
+      </StyledButton>
+      <StyledButton
+        className={currentFilter === FilterType.Completed ? 'current-filter' : ''}
+        data-filter="completed"
+        onClick={clickHandler}
+      >
+        Completed
+      </StyledButton>
     </StyledFieldSet>
   );
 };

5d3d45a7b6a001e88e178f1d80628173727cfa56
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-12 12:01:02 +0800

    Seperate FillContainer to another file

---
+++ src/styles/common/fillContainer.tsx
@@ -0,0 +1,11 @@
+import { css } from 'styled-components';
+
+const FillContainer = css`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+export default FillContainer;

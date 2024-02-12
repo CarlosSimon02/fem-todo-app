3caaa762a1d37d7e8cb0c7a23a7d24610ae92e9d
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-12 11:58:57 +0800

    Fix transition animating on first render

--- src/components/checkbox/checkbox.styles.tsx
+++ src/components/checkbox/checkbox.styles.tsx
@@ -1,55 +1,43 @@
-import styled, { css } from 'styled-components';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
 
-import { ReactComponent as CheckIcon } from '../../assets/icon-check.svg';
-import { ReactComponent as CircleIcon } from '../../assets/icon-circle.svg';
+import FillContainer from '../../styles/common/fillContainer';
 import { fluidSizeMobileToDesktop } from '../../styles/functions/fluidSize';
 import pxToRem from '../../styles/functions/pxToRem';
 
-export const StyledContainer = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 100%;
-  box-shadow: inset 0px 0px 0px 0px #56d0f5;
-  overflow: hidden;
-`;
+export const lightblue = 'hsl(192, 100%, 66.66666666666666%)';
 
-const StyledCircleIcon = css`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  left: 0;
-  top: 0;
-`;
-
-export const StyledDefaultCircleIcon = styled(CircleIcon)`
-  ${StyledCircleIcon}
+export const CheckContainerSVG = styled.svg`
+  ${FillContainer}
   stroke: ${({ theme }) => theme.color.checkbox.border};
 `;
 
-export const StyledCheckCircleIcon = styled(CircleIcon)`
-  ${StyledCircleIcon}
-  stroke: hsl(192, 100%, 66.66666666666666%);
-  rotate: -90deg;
+export const CheckIndicatorCircleSVG = styled.svg`
+  ${FillContainer}
 `;
 
-export const StyledCheckIcon = styled(CheckIcon)`
+export const AnimatedCheckCircleContainer = motion(styled.div`
+  ${FillContainer}
+  overflow: hidden;
+  border-radius: 100%;
+  box-shadow: inset 0 0 0 2rem ${lightblue};
+`);
+
+export const AnimatedGradient = motion(styled.div`
+  ${FillContainer};
+  border-radius: 100%;
+  background-image: linear-gradient(to bottom right, transparent, hsl(280.26, 86.59%, 64.9%));
+`);
+
+export const CheckIconSVG = styled.svg`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   width: ${pxToRem(9)};
   height: ${pxToRem(7)};
   z-index: 10;
 `;
-
-export const StyledGradient = styled.div`
-  ${StyledCircleIcon};
-  border-radius: 100%;
-  background-image: linear-gradient(to bottom right, transparent, hsl(280.26, 86.59%, 64.9%));
-`;
-
 export const StyledCheckboxButton = styled.button`
   cursor: pointer;
   width: ${pxToRem(21)};
@@ -62,7 +50,40 @@ export const StyledCheckboxButton = styled.button`
   border: none;
   padding: 0;
 
-  &:hover ${StyledDefaultCircleIcon} {
+  &:hover ${CheckContainerSVG} {
     stroke: ${({ theme }) => theme.color.mainBox.text};
   }
 `;
+
+// export const StyledContainer = styled.div`
+//   position: absolute;
+//   width: 100%;
+//   height: 100%;
+//   top: 0;
+//   left: 0;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   border-radius: 100%;
+//   box-shadow: inset 0px 0px 0px 0px #56d0f5;
+//   overflow: hidden;
+// `;
+
+// const StyledCircleIcon = css`
+//   width: 100%;
+//   height: 100%;
+//   position: absolute;
+//   left: 0;
+//   top: 0;
+// `;
+
+// export const StyledDefaultCircleIcon = styled(CircleIcon)`
+//   ${StyledCircleIcon}
+//   stroke: ${({ theme }) => theme.color.checkbox.border};
+// `;
+
+// export const StyledCheckCircleIcon = styled(CircleIcon)`
+//   ${StyledCircleIcon}
+//   stroke: hsl(192, 100%, 66.66666666666666%);
+//   rotate: -90deg;
+// `;

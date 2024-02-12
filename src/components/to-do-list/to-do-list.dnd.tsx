802237f87ebba0c88c0bed4e046ee5b5c14bc7f3
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-12 12:00:35 +0800

    Impelement drag and drop for to do items when filter is "all"

---
+++ src/components/to-do-list/to-do-list.dnd.tsx
@@ -0,0 +1,30 @@
+// import { DragDropContext, DropResult, Droppable, DroppableProvided, DroppableStateSnapshot } from 'react-beautiful-dnd';
+
+const DNDToDoList = () => {
+  // const dragEndHandler = (result: DropResult) => {
+  //   // dropped outside the list
+  //   if (!result.destination) {
+  //     return;
+  //   }
+
+  //   const items = reorder(this.state.items, result.source.index, result.destination.index);
+
+  //   this.setState({
+  //     items,
+  //   });
+  // };
+  return (
+    <div></div>
+    // <DragDropContext onDragEnd={dragEndHandler}>
+    //   <Droppable droppableId="droppable">
+    //     {(provided: DroppableProvided, snapshot: DroppableStateSnapshot) => {
+    //       return <div ref={provided.innerRef}
+    //       {...provided.draggableProps}
+    //       {...provided.dragHandleProps}></div>;
+    //     }}
+    //   </Droppable>
+    // </DragDropContext>
+  );
+};
+
+// export default DNDToDoList;

c648346c1beae61fb448a34c72537122b123037a
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-12 11:58:52 +0800

    Fix transition animating on first render

---
+++ src/components/checkbox/checkbox.props.tsx
@@ -0,0 +1,23 @@
+export type CheckBoxProps = {
+  checked?: boolean;
+  onClick?: () => void;
+};
+
+export type CheckIndicatorIconProps = {
+  isTransitionEnabled: boolean;
+};
+
+export const CircleProps = {
+  cx: '10.5',
+  cy: '10.5',
+  r: '9.5',
+  strokeWidth: '1.5',
+  fill: 'none',
+};
+
+export const CircleSVGProps = {
+  xmlns: 'http://www.w3.org/2000}svg',
+  viewBox: '0 0 21 21',
+  width: '21',
+  height: '21',
+};

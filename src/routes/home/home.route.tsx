0da636c4ecbc013759647bf26160b03f7a045b2a
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-13 21:07:16 +0800

    Update styles

--- src/routes/home/home.route.tsx
+++ src/routes/home/home.route.tsx
@@ -1,18 +1,22 @@
 import CreateToDo from '../../components/create-to-do/create-to-do.component';
 import FilterBar from '../../components/filter-bar/filter-bar.component';
 import ToDoList from '../../components/to-do-list/to-do-list.component';
+import { FilterType } from '../../contexts/to-do-items.context';
+import useToDoItems from '../../hooks/useToDoItems.hook';
 import useWindow from '../../hooks/useWindow.hook';
 import { BREAKPOINTS } from '../../styles/constants/breakpoints';
-import { StyledHomeContainer } from './home.styles';
+import { StyledDNDIndicator, StyledHomeContainer } from './home.styles';
 
 const Home = () => {
   const { innerWidth } = useWindow();
+  const { currentFilter } = useToDoItems();
 
   return (
     <StyledHomeContainer>
       <CreateToDo />
       <ToDoList />
       {innerWidth < BREAKPOINTS.UP.TABLET && <FilterBar />}
+      {FilterType.All === currentFilter && <StyledDNDIndicator>Drag and drop to reorder list</StyledDNDIndicator>}
     </StyledHomeContainer>
   );
 };
--- src/routes/home/home.styles.tsx
+++ src/routes/home/home.styles.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+import { fluidSizeMobileToDesktop } from '../../styles/functions/fluidSize';
 import pxToRem from '../../styles/functions/pxToRem';
 
 export const StyledHomeContainer = styled.div`
@@ -7,4 +8,9 @@ export const StyledHomeContainer = styled.div`
   display: grid;
   align-items: stretch;
   gap: ${pxToRem(16)};
+`;
+
+export const StyledDNDIndicator = styled.p`
+  text-align: center;
+  margin-top: ${fluidSizeMobileToDesktop(24, 33)};
 `;

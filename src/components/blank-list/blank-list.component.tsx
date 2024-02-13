b6724c280d5c8308f11afb5020abccfc1728c78b
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-13 21:08:33 +0800

    Add blank list component when when there is no items

---
+++ src/components/blank-list/blank-list.component.tsx
@@ -0,0 +1,11 @@
+import { StyledBlankListContainer } from './blank-list-styles';
+
+const BlankList = () => {
+  return (
+    <StyledBlankListContainer>
+      <p>There are no items to show</p>
+    </StyledBlankListContainer>
+  );
+};
+
+export default BlankList;

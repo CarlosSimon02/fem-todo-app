14b724245e8a61de4b0a6a9e8d5b1878ef20ad3c
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-14 20:22:11 +0800

    Update styles

--- src/components/bg-image/bg-image.styles.tsx
+++ src/components/bg-image/bg-image.styles.tsx
@@ -3,7 +3,10 @@ import styled, { css } from 'styled-components';
 import bgImageDarkJPG from '../../assets/bg-desktop-dark.jpg';
 import bgImageLightJPG from '../../assets/bg-desktop-light.jpg';
 import FillContainer from '../../styles/common/fillContainer';
+import { BREAKPOINTS } from '../../styles/constants/breakpoints';
 import { fluidSizeMobileToDesktop } from '../../styles/functions/fluidSize';
+import pxToEm from '../../styles/functions/pxToEm';
+import pxToRem from '../../styles/functions/pxToRem';
 
 export const StyledBGImageContainer = styled.div`
   width: 100%;
@@ -13,6 +16,15 @@ export const StyledBGImageContainer = styled.div`
   top: 0;
   left: 0;
   z-index: -99;
+  overflow: hidden;
+
+  @media (min-width: ${pxToEm(BREAKPOINTS.UP.DESKTOP)}) {
+    width: ${pxToRem(1350)};
+    left: 50%;
+    transform: translateX(-50%);
+    border-bottom-left-radius: ${({ theme }) => theme.borderRadius.medium};
+    border-bottom-right-radius: ${({ theme }) => theme.borderRadius.medium};
+  }
 `;
 
 const StyledImage = css`

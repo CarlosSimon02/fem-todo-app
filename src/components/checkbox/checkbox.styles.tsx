675f7f3e99d44b7ade32a96f76e46b9ad88712f3
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-14 20:22:16 +0800

    Update styles

--- src/components/checkbox/checkbox.styles.tsx
+++ src/components/checkbox/checkbox.styles.tsx
@@ -54,36 +54,3 @@ export const StyledCheckboxButton = styled.button`
     stroke: ${({ theme }) => theme.color.mainBox.text};
   }
 `;
-
-// export const StyledContainer = styled.div`
-//   position: absolute;
-//   width: 100%;
-//   height: 100%;
-//   top: 0;
-//   left: 0;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   border-radius: 100%;
-//   box-shadow: inset 0px 0px 0px 0px #56d0f5;
-//   overflow: hidden;
-// `;
-
-// const StyledCircleIcon = css`
-//   width: 100%;
-//   height: 100%;
-//   position: absolute;
-//   left: 0;
-//   top: 0;
-// `;
-
-// export const StyledDefaultCircleIcon = styled(CircleIcon)`
-//   ${StyledCircleIcon}
-//   stroke: ${({ theme }) => theme.color.checkbox.border};
-// `;
-
-// export const StyledCheckCircleIcon = styled(CircleIcon)`
-//   ${StyledCircleIcon}
-//   stroke: hsl(192, 100%, 66.66666666666666%);
-//   rotate: -90deg;
-// `;
